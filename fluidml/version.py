90fdecba032a159267b1718ba529614875b995bf
AlixBernard <alix.bernard9@gmail.com>
2022-06-10 15:04:44 +0700

    Revert "Initial commit"
    This reverts commit 0be8075e5cf5cbb835f7282222cf217149313b4a.

--- fluidml/version.py
+++
@@ -1,8 +0,0 @@
-# THIS FILE IS GENERATED FROM TBRF SETUP.PY
-short_version = '0.9.0'
-version = '0.9.0'
-full_version = '0.9.0.dev'
-git_revision = ''
-release = False
-if not release:
-    version = full_version
