045d730e90aee22ee0c52206c35cb733a0bbc96c
AlixBernard <alix.bernard9@gmail.com>
2023-03-05 12:22:38 +0700

    chore: modified classifiers

--- setup.py
+++ setup.py
@@ -79,12 +79,11 @@ setup(
     url="https://github.com/NotAssigned",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Fluid Dynamics researchers",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
-        "Topic :: Fluid Dynamics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Physics",
     ],
 )
