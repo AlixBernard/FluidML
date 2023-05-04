d88d66378700fa5dadb81e09950c2f8377339700
AlixBernard <alix.bernard9@gmail.com>
2023-05-04 11:24:54 +0700

    bugfix: solve circular import

--- fluidml/models/__init__.py
+++ fluidml/models/__init__.py
@@ -1,9 +1,9 @@
 __all__ = []
 
 
+from fluidml.models.tree import *
 from fluidml.models.tbdt import *
 from fluidml.models.tbrf import *
-from fluidml.models.tree import *
 
 __all__ += tree.__all__
 __all__ += tbdt.__all__
