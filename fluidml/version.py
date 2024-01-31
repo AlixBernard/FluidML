cadcc38db910b0b20d4c2821ddeac3de3bad7774
Alix Bernard <alix.bernard9@gmail.com>
2024-01-31 12:57:48 +0700

    fix: set automatic version detection via `pkg_resources`

--- fluidml/version.py
+++ fluidml/version.py
@@ -1,8 +1,3 @@
-# THIS FILE IS GENERATED FROM FLUIDML SETUP.PY
-short_version = '0.10.0'
-version = '0.10.0'
-full_version = '0.10.0.dev'
-git_revision = ''
-release = False
-if not release:
-    version = full_version
+import pkg_resources
+
+version = pkg_resources.get_distribution("fluidml").version
